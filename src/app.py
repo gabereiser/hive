e8fe12aec7b99783c4e8f987482c3cf6f1447038
Gabriel Reiser <gabe@gabereiser.com>
2019-10-26 10:08:34 -0600

    Initial controller and page structure (#5)
    * Initial controller and page structure
    
    * Fix circleci

--- src/app.py
+++ src/app.py
@@ -1,6 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from controllers import home
+import os
 
-def main():
-  print "Hello World"
+app = Flask(__name__, static_url_path='',
+               static_folder='static',
+               template_folder='templates')
+
+DB_URI = os.getenv("DATABASE_URI") or 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+app.register_blueprint(home.home_blueprint)
 
 if __name__ == "__main__":
-  main()
+    app.run(port=8080)
